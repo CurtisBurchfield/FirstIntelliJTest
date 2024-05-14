d763465e3d5dbb98462b36d7cb1a925baef58320
CurtisBurchfield <burchfieldcurtis@gmail.com>
2024-05-13 23:17:43 -0700

    Third commit, another git test

--- src/HelloThere.java
+++ src/HelloThere.java
@@ -2,5 +2,6 @@ public class HelloThere {
     public static void main(String[] args) {
         System.out.println("Hello there -Kenobi");
         System.out.println("Git Test");
+        System.out.println("Git Test 2");
     }
 }
